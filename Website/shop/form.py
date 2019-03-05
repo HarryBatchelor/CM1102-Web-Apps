bcd5918cdbe18f0780010340772ade0630f12bcf
HarryBatchelor <hmbatchelor8@gmail.com>
2019-03-05 13:50:13 +0000

    Flask is getting there

--- Website/shop/form.py
+++ Website/shop/form.py
@@ -2,6 +2,7 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo
 
+
 class RegistrationForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired(), Length(min=3, max=15)])
     email = StringField('Email',
@@ -15,3 +16,8 @@ class LoginForm(FlaskForm):
     email = StringField('Email', validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
     submit = SubmitField('Login')
+
+# def validate_email(self, email):
+#     user = User.query.filter_by(email=email.data).first()
+#     if user:
+#         rais ValidationError('This email is already registered.\ please choose a differnt one')
