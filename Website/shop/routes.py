9b49b2a87c19a5a109c0a9fb18a732482d19545f
Harry Batchelor <batchelorh@cardiff.ac.uk>
2019-03-12 16:29:26 +0000

    almost done!!!!

--- Website/shop/routes.py
+++ Website/shop/routes.py
@@ -9,12 +9,19 @@ from flask_login import login_user, current_user, logout_user, login_required
 @app.route("/")
 @app.route("/home")
 def home():
+    if request.method =='POST':
+        sorting_post = request.form["sorting"]
+        split_sorting_post = sorting_post_split("_")
+        split_sorting_post[0]
+        method = split_sorting_post[1]
+        items = db.engine.execute("SELECT * FROM item ORDER BY" + str(sort) + " " + str(method))
+        return render_template('home.html', items=items)
     items = Item.query.all()
     return render_template('home.html', items=items, title='My Shop')
 
-@app.route("/account")
-def account():
-    return render_template('account.html', title='Account')
+@app.route("/finished")
+def finished():
+    return render_template('finished.html', title='Thank you')
 
 @app.route("/checkout")
 def checkout():
