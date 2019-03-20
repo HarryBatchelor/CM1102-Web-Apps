a01ed8c650a150051d604ade3cbf91c2495029c7
HarryBatchelor <hmbatchelor8@gmail.com>
2019-03-20 19:10:11 +0000

    end me

--- Website/shop/routes.py
+++ Website/shop/routes.py
@@ -11,12 +11,12 @@ from flask_login import login_user, current_user, logout_user, login_required
 def home():
     if request.method == 'POST':
         sorting_post = request.form["sorting"]
-        split_sorting_post = sorting_post.split("_")
+        split_sorting_post = sorting_post.split("-")
         sort = split_sorting_post[0]
         method = split_sorting_post[1]
         items = db.engine.execute("SELECT * FROM item ORDER BY " + str(sort) + " " + str(method))
-        return render_template('home.html', itmes=items)
-    items = Item.query.limit(20).all()
+        return render_template('home.html', items=items)
+    items = db.engine.execute("SELECT * FROM item LIMIT 20")
     return render_template('home.html', items=items)
 @app.route("/search", methods=['GET', 'POST'])
 def search():
